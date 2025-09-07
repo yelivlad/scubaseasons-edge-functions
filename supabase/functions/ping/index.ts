82d4a1cf5f9d7b689c2d262d44db2379fd134ae6
vladimir el <ext_vladimir@yad2.co.il>
2025-09-07 07:35:58 -0600

    docs to a function

--- supabase/functions/ping/index.ts
+++ supabase/functions/ping/index.ts
@@ -2,6 +2,20 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { createClient } from "jsr:@supabase/supabase-js@2";
 import { json } from "@shared/utils.ts";
 
+/**
+  * A simple ping function to test if the function is working.
+  * It also tests if the database connection is working by querying the `notifications` table.
+  * If the database connection is not configured, it will skip the database test.
+  * Returns a JSON response with the status of the function and the database connection.
+  * Example response:
+  * { ok: true, data: [...], dbStatus: "ok", myMsg: "test2" }
+  * or if the database connection is not configured:
+  * { ok: true, data: null, dbStatus: "skipped", myMsg: "test2" }
+  * or if there is an error:
+  * { ok: false, message: "error message" }
+  * 
+ */
+
 export const handler = async (req: Request) => {
   try {
     const SUPABASE_URL = Deno.env.get("SUPABASE_URL");
